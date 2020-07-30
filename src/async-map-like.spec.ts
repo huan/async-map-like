f424279ba20d941eefaad43d02061d93726d4201
Huan LI (李卓桓) <zixia@zixia.net>
2020-07-30 11:32:44 +0800

    fix AsyncMapLike typing

--- src/async-map-like.spec.ts
+++ src/async-map-like.spec.ts
@@ -4,7 +4,7 @@ import { test }  from 'tstest'
 
 import { AsyncMapLike } from './async-map-like'
 
-test('AsyncMapLike Interface', async (t) => {
+test('AsyncMapLike Interface via object', async (t) => {
   const mapCollection = {
     clear: async (): Promise<void> => {},
     delete: async (key: any): Promise<boolean> => { return !!key },
@@ -35,4 +35,47 @@ test('AsyncMapLike Interface', async (t) => {
   }
 
   t.ok(mapLike, 'should be assign-able from ES6 Map to our MapLike')
+})
+
+test('AsyncMapLike Interface via class', async (t) => {
+  class TestAsyncMapLike implements AsyncMapLike<string, number> {
+
+    constructor () {}
+
+    /**
+     * Collection
+     */
+    async clear (): Promise<void> {}
+    async delete (key: string): Promise<boolean> { return !!key }
+    async forEach (
+      callbackfn: (
+        value: number,
+        key: string,
+        // map: TestAsyncMapLike,
+        // FIXME(huan) 202007: we have to use any at here, because the typing system is very hard to
+        //  rename `Map` to `TestAsyncMapLike` in this method function parameters.
+        map: any,
+      ) => void,
+      thisArg?: any,
+    ): Promise<void> { void callbackfn; void thisArg }
+
+    async get (key: string): Promise<number> { void key; return 42 }
+    async has (key: string): Promise<boolean> { return !!key }
+    async set (key: string, value: number): Promise<this> { void key; void value; return this }
+    get size () { return Promise.resolve(42) }
+
+    /**
+     * Interable
+     */
+    [Symbol.iterator] (): AsyncIterableIterator<[string, number]> { return {} as any }
+    entries (): AsyncIterableIterator<[string, number]> { return {} as any }
+    keys (): AsyncIterableIterator<string> { return {} as any }
+    values (): AsyncIterableIterator<number> { return {} as any }
+
+    get [Symbol.toStringTag] () { return 'test' }
+
+  }
+
+  const mapLike = new TestAsyncMapLike()
+  t.ok(mapLike, 'should be implement-able from AsyncMapLike')
 })
--- src/async-map-like.ts
+++ src/async-map-like.ts
@@ -1,13 +1,13 @@
-type AsyncifyIterable<K, V, R extends any> = R extends IterableIterator<[K, V]>
-  ? AsyncIterableIterator<[K, V]>
-  : R extends boolean
-    ? Promise<boolean>
+type AsyncifyIterable<R extends any> = R extends IterableIterator<infer I>
+  ? AsyncIterableIterator<I>
+  : R extends Map<infer K, infer V> ? Promise<AsyncMapLike<K, V>>
+    : R extends boolean ? Promise<boolean>
     : Promise<R>
 
-type AsyncifyFunction<K, V, R extends any> = R extends (...args: any) => any
-  ? (...args: Parameters<R>) => AsyncifyIterable<K, V, ReturnType<R>>
+type AsyncifyFunction<R extends any> = R extends (...args: any) => any
+  ? (...args: Parameters<R>) => AsyncifyIterable<ReturnType<R>>
   : Promise<R>
 
 export type AsyncMapLike<K, V> = {
-  [key in keyof Map<K, V>]: AsyncifyFunction<K, V, Map<K, V>[key]>
+  [key in keyof Map<K, V>]: AsyncifyFunction<Map<K, V>[key]>
 }
